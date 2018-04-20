b126f3346b07a64d580457e2e8e579e48494def4
Agata Biesaga <abiesaga@gmail.com>
2018-04-20 21:06:51 +0200

    Dodano alternatywne rozwiązanie.

--- 29-js-suma-zmiennych/js/main.js
+++ 29-js-suma-zmiennych/js/main.js
@@ -11,3 +11,15 @@ alert(sumaZmiennych(liczbaPierwsza, liczbaDruga));
 //lub
 
 alert(sumaZmiennych(10, 3));
+
+//lub
+
+var liczbaPierwsza = 10;
+var liczbaDruga = 3;
+
+function sumaZmiennych(x, y) {
+    var suma = x + y;
+    return suma;
+}
+
+alert(sumaZmiennych(liczbaPierwsza, liczbaDruga));
