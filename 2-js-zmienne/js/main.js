1444fa1bc27a0f05cf64760ebed56026faa7672a
Agata Biesaga <abiesaga@gmail.com>
2018-04-22 10:24:53 +0200

    Ćwiczenie "2-js-zmienne".

---
+++ 2-js-zmienne/js/main.js
@@ -0,0 +1,13 @@
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+var kolor = "żółty";
+
+imie = "Sergio";
+
+imie = imieDamskie;
+
+console.log(imie);
+console.log(imieDamskie);
+console.log(wiek);
